f28ca29f7746ec55f1966c3cf2803e1f406c0d84
Ankit Maheshwari <maheshwari.ankit@gmail.com>
2021-11-21 22:44:27 +0530

    Initialise

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    semi: [2, 'never'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none', // 'none' or 'semi' or 'comma'
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi', // 'semi' or 'comma'
+          requireLast: false,
+        },
+      },
+    ],
+  },
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import './sass/style.scss'
+
+const App = () => {
+  return <h1>Hello</h1>
+}
+ReactDOM.render(<App />, document.querySelector('#root'))
